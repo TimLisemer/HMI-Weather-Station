fd1774b20d2992028f768748f208e0f70b701575
Anastasia <anastasia.tsaava@stud.h-da.de>
2024-05-21 12:15:00 +0200

    Test für sensor

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <qthread.h>
 #include <iostream>
 #include <wiringPi.h>
 
+#define PIN_LSENSOR 4
+
 int main(int argc, char *argv[])
 {
-
+    wiringPiSetupGpio();
+    pinMode(PIN_LSENSOR, OUTPUT);
 
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
@@ -19,6 +23,16 @@ int main(int argc, char *argv[])
             QCoreApplication::exit(-1);
     }, Qt::QueuedConnection);
     engine.load(url);
+
+    while (true) {
+        if (digitalRead(PIN_LSENSOR) == LOW){
+            std::cout<<"LOW!"<<std::endl;
+        }
+        else {
+            std::cout<<"HIGH!"<<std::endl;
+        }
+        QThread::msleep(10);
+    }
 
     return app.exec();
 }
